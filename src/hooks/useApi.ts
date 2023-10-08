c931f124b142da2baac991b202ebeaa26fcb4553
Rubén Chiquin <randreu28@gmail.com>
2023-10-08 11:33:51 +0200

    Creates a custom useAPI hoook

---
+++ src/hooks/useApi.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const baseUrl = "http://localhost:3001/api";
+
+export default function useApi<T>(endpoint: string) {
+  const [data, setData] = useState<T[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>();
+
+  useEffect(() => {
+    setIsLoading(true);
+    axios
+      .get<T[]>(baseUrl + endpoint)
+      .then(({ data }) => setData(data))
+      .catch((e) => setError(e));
+
+    setIsLoading(false);
+  }, [endpoint]);
+
+  return {
+    data,
+    isLoading,
+    error,
+  };
+}
