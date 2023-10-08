d9dd6947886e6703ba2d70cf57aa301315265dfd
Rubén Chiquin <randreu28@gmail.com>
2023-10-08 13:24:11 +0200

    Moves the <AuthProvider/> to main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import AuthProvider from "./components/AuthProvider";
 import BlogList from "./components/BlogList";
 
 export default function App() {
   return (
-    <AuthProvider>
-      <div className="p-5">
-        <h1 className="text-center text-3xl">Blogs</h1>
-        <BlogList />
-      </div>
-    </AuthProvider>
+    <div className="p-5">
+      <h1 className="text-center text-3xl">Blogs</h1>
+      <BlogList />
+    </div>
   );
 }
