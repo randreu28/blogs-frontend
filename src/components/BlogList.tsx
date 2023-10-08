67acd875f13a26444c8bde1a3b76bb5f7d9757f1
Rubén Chiquin <randreu28@gmail.com>
2023-10-08 20:00:11 +0200

    Sorts blogs by likes

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -17,9 +17,11 @@ export default function BlogList() {
 
   return (
     <>
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} />
-      ))}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} />
+        ))}
     </>
   );
 }
