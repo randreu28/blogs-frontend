0ca5185d78e654901d66a8639b8b3e54892c2886
Rubén Chiquin <randreu28@gmail.com>
2023-10-08 16:28:29 +0200

    Installs sonner as a dependency

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import AuthProvider from "./components/AuthProvider.tsx";
+import { Toaster } from "sonner";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider>
+      <Toaster richColors />
       <App />
     </AuthProvider>
   </React.StrictMode>,
