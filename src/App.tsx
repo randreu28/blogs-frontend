2027ffe4a04d40e4687b49bdb41cbff5c0b6d9b7
Rubén Chiquin <randreu28@gmail.com>
2023-10-08 11:34:12 +0200

    Makes a simple list of blogs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import Blog from "./components/Blog";
+import useApi from "./hooks/useApi";
+import { BlogType } from "./types";
+
 export default function App() {
+  const { data: blogs, error, isLoading } = useApi<BlogType>("/blogs");
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    <p>loading</p>;
+  }
+
   return (
-    <div className="flex h-screen items-center justify-center text-3xl">
-      Hello world
+    <div className="p-5">
+      <h1 className="text-center text-3xl">Blogs</h1>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
     </div>
   );
 }
---
+++ src/components/Blog.tsx
@@ -0,0 +1,17 @@
+import type { BlogType } from "../types";
+
+type Props = {
+  blog: BlogType;
+};
+
+export default function Blog({ blog }: Props) {
+  return (
+    <div className="space-y-1 p-5">
+      <h2 className="text-xl">{blog.title}</h2>
+      <p className="text-sm text-gray-500">
+        <b>Author: </b>
+        {blog.author}
+      </p>
+    </div>
+  );
+}
