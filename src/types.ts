545adb818e6c530c0c5735acc8e45e06fe9934de
Rubén Chiquin <randreu28@gmail.com>
2023-10-08 11:33:28 +0200

    Creates the types

---
+++ src/types.ts
@@ -0,0 +1,27 @@
+export type BlogType = {
+  title: string;
+  author: string;
+  url: string;
+  likes: number;
+  user: {
+    username: string;
+    name: string;
+    blogs: string[];
+    id: string;
+  };
+  id: string;
+};
+
+export type UserType = {
+  username: string;
+  name: string;
+  blogs: {
+    title: string;
+    author: string;
+    url: string;
+    likes: number;
+    user: string;
+    id: string;
+  };
+  id: string;
+};
